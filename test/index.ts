44fa649f858b65544fee853e0cbdb6ee9d8c7a66
Teselka <savi_2005@mail.ru>
2022-06-25 15:52:29 +0300

    .npmrc ignore and github packages publish

--- test/index.ts
+++ test/index.ts
@@ -3,4 +3,5 @@ import { TelegramMethods as M, TelegramTypes as T } from '../lib/index';
 // some typescript usage to verify that our library is working
 type chat = T.Chat;
 type user = T.User;
+
 type getMe = M.getMe;
