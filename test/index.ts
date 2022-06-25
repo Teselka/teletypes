ef2157ef87dd4180b1199499f64b9ca0434769c2
Teselka <savi_2005@mail.ru>
2022-06-25 15:34:56 +0300

    pretty tests

--- test/index.ts
+++ test/index.ts
@@ -1,24 +1,24 @@
-import { TelegramMethods as Methods, TelegramTypes as Types } from '../lib/index';
+import { TelegramMethods as M, TelegramTypes as T } from '../lib/index';
 
 // some typescript usage to verify that our library is working
-const chat: Types.Chat = {
+const chat: T.Chat = {
     id: 0,
     type: 'yes'
 };
 
-const user: Types.User = {
+const user: T.User = {
     id: 0,
     first_name: 'yes',
     is_bot: false
 }
 
-const msg: Types.Message = {
+const msg: T.Message = {
     message_id: 0,
     date: 0,
     chat: chat
 };
 
-const fn: Methods.getMe = () => { return {
+const fn: M.getMe = () => { return {
     id: 0,
     is_bot: true,
     first_name: 'yes'
