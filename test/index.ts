da1bc82b8f3d5256a1210c1d790366d4dfb467dd
Teselka <savi_2005@mail.ru>
2022-06-25 15:36:48 +0300

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -1,25 +1,6 @@
 import { TelegramMethods as M, TelegramTypes as T } from '../lib/index';
 
 // some typescript usage to verify that our library is working
-const chat: T.Chat = {
-    id: 0,
-    type: 'yes'
-};
-
-const user: T.User = {
-    id: 0,
-    first_name: 'yes',
-    is_bot: false
-}
-
-const msg: T.Message = {
-    message_id: 0,
-    date: 0,
-    chat: chat
-};
-
-const fn: M.getMe = () => { return {
-    id: 0,
-    is_bot: true,
-    first_name: 'yes'
-}; };
+type chat = T.Chat;
+type user = T.User;
+type getMe = M.getMe;
